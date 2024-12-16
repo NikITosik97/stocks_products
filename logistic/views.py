524f4cace663347a82611acb5e1b61e87af719d1
Nikita <minenkov07@mail.ru>
2024-12-16 15:51:18 +0700

    Change sample view

--- logistic/views.py
+++ logistic/views.py
@@ -1,5 +1,7 @@
+from rest_framework.decorators import api_view
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
@@ -19,3 +21,8 @@ class StockViewSet(ModelViewSet):
     serializer_class = StockSerializer
     filterset_fields = ['products']
     pagination_class = LimitOffsetPagination
+
+
+@api_view(['GET'])
+def sample_view(requests):
+    return Response({'massage': 'Hello'})
